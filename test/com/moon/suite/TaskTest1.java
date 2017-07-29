844501111cb91116748c59371f205faf7b7881b4
moon-zhou <ayimin1989@163.com>
2017-07-29 09:50:06 +0800

    Junit demo
    suite

---
+++ test/com/moon/suite/TaskTest1.java
@@ -0,0 +1,12 @@
+package com.moon.suite;
+
+import org.junit.Test;
+
+public class TaskTest1 {
+
+	@Test
+	public void test() {
+		System.out.println("this is TaskTest1...");
+	}
+
+}
---
+++ test/com/moon/suite/TaskTest2.java
@@ -0,0 +1,12 @@
+package com.moon.suite;
+
+import org.junit.Test;
+
+public class TaskTest2 {
+
+	@Test
+	public void test() {
+		System.out.println("this is TaskTest2...");
+	}
+
+}
